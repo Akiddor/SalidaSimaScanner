f455c29b726f1f0a21a42c015aa6bdb7f2e49314
Akiddor <Akiddor7@gmail.com>
2025-01-03 12:03:49 -0600

    Change in perfomance for add_items_calidad

--- Calidad/add_item_calidad/add_item_calidad.php
+++ Calidad/add_item_calidad/add_item_calidad.php
@@ -2,6 +2,24 @@
 require '../../backend/db/db.php';
 
 $day_id = isset($_GET['day_id']) ? intval($_GET['day_id']) : 0;
+
+// Obtener la fecha correspondiente al day_id desde la base de datos
+$day_query = "SELECT day_date FROM calidad_days WHERE id = $day_id";
+$day_result = mysqli_query($enlace, $day_query);
+$day_fecha = '';
+if ($day_result && mysqli_num_rows($day_result) > 0) {
+    $day_row = mysqli_fetch_assoc($day_result);
+    $day_fecha = $day_row['day_date'];
+}
+
+// Obtener el último número de serie registrado
+$last_serial_query = "SELECT serial_number FROM calidad_cajas_scanned WHERE DATE(scan_timestamp) = '$day_fecha' ORDER BY scan_timestamp DESC LIMIT 1";
+$last_serial_result = mysqli_query($enlace, $last_serial_query);
+$last_serial = '';
+if ($last_serial_result && mysqli_num_rows($last_serial_result) > 0) {
+    $last_serial_row = mysqli_fetch_assoc($last_serial_result);
+    $last_serial = $last_serial_row['serial_number'];
+}
 ?>
 
 <!DOCTYPE html>
@@ -16,11 +34,19 @@ $day_id = isset($_GET['day_id']) ? intval($_GET['day_id']) : 0;
 <body>
     <div class="container">
         <h2>Agregar Ítem | CALIDAD</h2>
+        <h2>Estas en el Dia: <?php echo htmlspecialchars($day_fecha); ?></h2>
 
         <!-- Mostrar mensaje de éxito o error si está presente -->
         <?php if (isset($_GET['message']) && isset($_GET['messageType'])): ?>
             <div id="notification" class="notification <?php echo htmlspecialchars($_GET['messageType']); ?> show">
                 <?php echo htmlspecialchars($_GET['message']); ?>
+            </div>
+        <?php endif; ?>
+
+        <!-- Mostrar el último número de serie registrado -->
+        <?php if ($last_serial): ?>
+            <div id="last-serial" class="notification info show">
+                Último número de serie registrado: <?php echo htmlspecialchars($last_serial); ?>
             </div>
         <?php endif; ?>
 
